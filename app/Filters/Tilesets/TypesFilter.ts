53e314d2b4986dc328380c3591c0cd88a65a623a
elGerardo <gerardoyu54@gmail.com>
2023-07-25 13:11:55 -0700

    fixed little filter issue

--- app/Filters/Tilesets/TypesFilter.ts
+++ app/Filters/Tilesets/TypesFilter.ts
@@ -1,7 +1,7 @@
 export default class TypesFilter {
     filter(query, params){
         try{
-            params = JSON.parse(params)
+            JSON.parse(params)
         }catch(error) { params = JSON.stringify(["MultiPolygon", "MultiLineString", "Point"]) }
         query.whereHas('geometry', builder => {
             builder.whereIn('type', JSON.parse(params))
